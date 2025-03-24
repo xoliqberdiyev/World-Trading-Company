5736db66bafcf62175752e205c3061a6aa56824d
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-03-24 18:07:19 +0500

    fix

--- migrations/000021_change_sub_category.up.sql
+++ migrations/000021_change_sub_category.up.sql
@@ -1,7 +1,7 @@
-ALTER TABLE products
-DROP CONSTRAINT fk_sub_category;
+-- ALTER TABLE products
+-- DROP CONSTRAINT fk_sub_category;
 
-ALTER TABLE products
-ADD CONSTRAINT fk_sub_category FOREIGN KEY (sub_category_id)
-REFERENCES sub_categories(id)
-ON DELETE SET NULL;
+-- ALTER TABLE products
+-- ADD CONSTRAINT fk_sub_category FOREIGN KEY (sub_category_id)
+-- REFERENCES sub_categories(id)
+-- ON DELETE SET NULL;
